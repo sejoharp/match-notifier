c06bf68dfbccd992e9dbc5e511cd57be70fe7b8c
Joscha Harpeng <joscha@harpeng.org>
2017-09-10 19:18:59 +0200

    add functions for test resources

--- test/match_notifier/core_test.clj
+++ test/match_notifier/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [match-notifier.core :refer :all]))
 
+(def overview-path "test/resources/overview.html")
+
+(def tournament-path "test/resources/tournament.html")
+
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
