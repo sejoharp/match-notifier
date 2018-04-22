c045660271fb0041d7564b5cfbc9099730a92a36
joscha <zippelman@web.de>
2018-04-22 12:54:30 +0200

    use clojure 1.9.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [hickory "0.7.1"]
                  [falcon "0.1.0"]]
   :main ^:skip-aot match-notifier.core
